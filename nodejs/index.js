5932b609e8d7bc92fb0178f069a4c02d96846b5e
moh <mohamadjb@gmail.com>
2020-05-18 09:37:54 +0300

    updates

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,11 @@
 now=()=>new Date().getTime();
 b64e=s=> Buffer.from(s).toString('base64');
 b64d=s=> new Buffer(s,'base64').toString();
-Date.prototype.toArray=function DateToArray(){let x=this;return [x.getFullYear(),x.getMonth()+1,x.getDay(),x.getHours(),x.getMinutes(),x.getSeconds(),x.getMilliseconds()];}
+Date.fields=['FullYear','Month','Day','Hours','Minutes','Seconds','Milliseconds']
+Date.fromArray=function DateFromArray(d,i){let x=new Date(0);
+	Date.fields.map((f,i)=>x['set'+f](d[i]-(i==1?1:0)));return x;}
+Date.prototype.toArray=function DateToArray(){
+	let x=this;return Date.fields.map((f,i)=>x['get'+f]()+(i==1?1:0));}
 const express=require('express');
 const app=express();
 const fs=require('fs');
@@ -74,33 +78,112 @@ function hash(p){
 } // function hash
 /////////////////////////////////
 
+let db={db:{},log:{bounds:[0,0]},session:{}
 
-
-let db={db:{},session:{}
-//,all:(  )=>new Promise((resolve,reject)=>{pool.query('select * from prop',(err,result)=>{if(err)return reject(err);return resolve(result);})})
-,init:()=>{
-	for(var i in result){
-		let o=result[i]
-		,cat=db.db[o.cat]
-		if(!cat)
-			cat=db.db[o.cat]={}
-		let id=cat[o.id]
-		if(!id)
-			id=cat[o.id]={cat:o.cat,id:o.id,log:o.log}
-		let pp=id[o.prop]
-		id[o.prop]=JSON.parse(o.val);
+,init:()=>{let log=db.log.bounds;
+	function f(pathArr){
+		function fil(pth,obj,prop){
+			fs.promises.readFile(pth,'utf-8').
+			then(x=>{if(prop=='meta')
+				obj.meta=JSON.parse(x)
+				else obj.val[prop]=JSON.parse(x)
+				fs.promises.lstat(pth,'utf-8').
+				then(x=>db.loglog(obj,prop,null,x.mtimeMs))
+			})
+		}
+		fs.promises.readdir(path,{withFileTypes:true}).then(a=>{
+			for (var i=0;i<a.length;i++) {
+				//console.log(file);
+				let x=a[i]
+				if(x.isDirectory())
+					f(pathArr.concat(x.name))
+				else{let n=pathArr.length
+					,catId=pathArr[n-3]
+					,id=pathArr[n-2]
+					,prop=pathArr[n-1]
+					,cat=db.db[catId],id,prop,obj,pth;
+					if(!cat)cat=db.db[catId]={}
+					pth=pathArr.concat(x.name).join('/');
+					obj=cat[id];if(!obj)
+						obj=cat[id]={cat:catId,id:id,log:0,meta:{},val:{},propLog:{}}
+					fil(pth,obj,prop);
+				}
+			}
+		})
 	}
+	f(['.','db','prop']);
 }
+,loglog:(obj,prop,da,lm)=>{
+	function rem(g){
+		let p=g.parent;
+		if(!p)return;
+		delete p[g.key];
+		let k=Object.keys(p)
+		if(k.length<=3)
+			rem(p);}
+	if(!da)da=new Date(lm||obj.log).toArray();
+	if(!lm)lm=obj.log;else
+	if(obj.log<lm)obj.log=lm;
+	if(obj.propLog[prop]){
+		rem(obj.propLog[prop])
+		obj.propLog[prop]=null;
+	}
+	let g=[db.log[da[0]]],i=0
+	while(i<da.length){
+		g[i]=g[i][da[i]]
+		if(!g[i]){g[i]=
+			{parent:g[i-1],key:da[i],t:Date
+				.fromArray(da,i).getTime()}
+			if(i==0){
+				db.log[da[0]]=g[0];
+				g[0].parent:db.log}
+			}
+		i++;}
+	g=g[g.length-1]
+	if(g.obj){i=da.length-1;
+		let p=g.parent,x=p[++da[i]]
+		while(x)
+			x=p[++da[i]];
+		p[da[i]]=g={parent:g[i-1],key:da[i]
+			,t:Date.fromArray(da,i).getTime()}
+	}
+	g.obj=obj;
+	g.prop=prop;
+	g.da=da;g.t=lm;
+	if(obj.propLog[prop])
+		rem(g);
+	obj.propLog[prop]=g
+	if(!log[0] || log[0].t>lm)
+		log[0]=g;		// TODO: if log[0] is relocated , must find the next minimum
+	if(!log[1] || log[1].t<lm)
+		log[1]=g;		// TODO: if log[1] is relocated, must find the previous maximum
+	}
+,byDate:(dt)=>{
+	let o={} // ,d=new Date(dt).toArray(),
+	function f(p,a,lvl){
+		for(let k in p){let g=p[k]
+			if(dt>g.t){
+				if(!g.obj)//lvl<7)
+					f(g,a.concat(k),lvl+1)
+				else{let x=g.obj
+					,cat=o[x.cat]||(o[x.cat]={})
+					,id=cat[x.id]||(cat[x.id]={})
+					if(!id[x.prop] || id[x.prop].t<x.t)
+					id[x.prop]={t:x.t,val:db.db[x.cat][x.id].val[x.prop]}
+				}
+			}
+		}
+	}
+	f(db.log,[],0)
+	return o;
+	}
 
 ,save:(path, obj,prop,dirdate)=>
 	fs.promises.mkdir('./db/prop/'+path,{recursive:true}).
-	then(()=>
-	fs.promises.writeFile('./db/prop/'+path+'/'+fnm
-	, JSON.stringify(content)));
-
-,load:(path,obj,prop,dirdate)=>
-	fs.promises.readFile('./db/prop/'+path+'/'+prop,'utf-8').
-	then(x=>obj[prop]=JSON.parse(x));
+		then(()=>
+		fs.promises.writeFile('./db/prop/'+path+'/'+fnm
+		, JSON.stringify(obj.val[prop])))
+//,load:(path,obj,prop,dirdate)=>fs.promises.readFile('./db/prop/'+path+'/'+prop,'utf-8').then(x=>obj[prop]=JSON.parse(x))
 
 ,access:(cid,cat,id)=>{//returnValue::= 0:no-access , 1:read-only , 2:write , 3:admin , 4:access
 	let r=0,d=db.db,a=[d[cat]],m
@@ -115,14 +198,6 @@ let db={db:{},session:{}
 		}
 	}return r;
 }
-,byDate:(dt)=>{
-	pool.query('select * from prop where `log`>?'
-		,dt,(err,result)=>{
-		if(err)
-			return reject(err)
-		return resolve(result);
-	})
-}
 ,put:(cat,id,prop,val)=>{
 	pool.query('replace into prop values(now(),?,?,?,?)'
 	,[cat,id,meta,val],(err,result)=>{
@@ -131,17 +206,25 @@ let db={db:{},session:{}
 		return resolve(result);
 	})}
 ,post:(cat,id,prop,val)=>{
-	pool.query('insert into prop values(now(),?,?,?,?)'
-	,[cat,id,meta,val],(err,result)=>{
-		if(err)
-			return reject(err)
-		return resolve(result);
-	})}
-
-,log:(cat,id,prop,meta,val)=>{
-	let d=new Date().toArray().;
-	if(!d)d=db.log.d=date=>{let d=[date.];};
+	if(db.db[cat] && db.db[cat][id])return null;
+	if(!db.db[cat])db.db[cat]={}
+	let o=db.db[cat][id]={cat:cat,id:id,log:now(),val:{}}
+	o.val[prop]=val;
+	db.save(cat+'/'+id,o,prop)
+	db.log(cat,id,prop,{op:'post',cid:null},val)
+	db.loglog(o,prop)
+	return o;
 	}
+,log:(cat,id,prop,meta,val)=>{
+	let t=new Date()
+	,da=t.toArray(),d=da.join('/')
+	,s='./db/log/'+d+'/'+cat+'/'+id+'/';
+	fs.promises.mkdir(s,{recursive:true}).then(()=>
+		fs.promises.writeFile(s+prop, JSON.stringify({
+			cat:cat,id:id,log:t.getTime()
+			,meta:meta,prop:prop,content:content
+		})));
+	db.loglog(db.db[cat][id],prop,da);}
 };//db
 
 
